413003ab3e324bd00d174bf3ce5e48c6f183bae4
Dazhy Khovalyg <hovalyg_d@mail.ru>
2025-02-13 23:38:18 +0300

    some additions

--- load_testing.go
+++ load_testing.go
@@ -2,33 +2,59 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 	vegeta "github.com/tsenart/vegeta/v12/lib"
 )
 
+func generateRandomURL() string {
+	// Инициализация генератора случайных чисел
+	rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	// Генерируем случайную строку для URL
+	randomStr := randStringBytes(10) // длина строки будет 10 символов
+
+	// Возвращаем сгенерированный URL
+	return fmt.Sprintf("https://example.com/%s", randomStr)
+}
+
+// Генерация случайной строки длиной n символов
+func randStringBytes(n int) string {
+	const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
+
+func myTarget(t *vegeta.Target) error {
+	// Генерация случайного URL
+	randomStr := randStringBytes(10) // длина строки будет 10 символов
+	randomURL := fmt.Sprintf("https://example.com/%s", randomStr)
+
+	// Заполняем Target
+	t.Method = "POST"
+	t.URL = "http://localhost:8080/shorten" // REST API
+	t.Body = []byte(fmt.Sprintf(`"%s"`, randomURL))
+
+	// Возвращаем nil, если ошибок нет
+	return nil
+}
+
 func main() {
-	// Настроим параметры нагрузки
-	target := "http://localhost:8080/shorten" // REST API
-
-	// Инициализация вектора нагрузочного тестирования
-	targeter := vegeta.NewStaticTargeter(vegeta.Target{
-		Method: "POST",
-		URL:    target,
-		Body: []byte(`"https://example.com"`),
-	})
-
-	// Настройка интенсивности запросов (например, 100 запросов в секунду)
+	// Настройка интенсивности запросов
 	rate := vegeta.Rate{Freq: 500, Per: time.Second}
 	
-	// Длительность теста (например, 10 секунд)
-	duration := 10 * time.Second
+	// Длительность теста
+	duration := 1 * time.Hour
 
 	// Создаём атакующего (attacker)
 	attacker := vegeta.NewAttacker()
 
 	// Запускаем нагрузочный тест
 	var metrics vegeta.Metrics
-	for res := range attacker.Attack(targeter, rate, duration, "Load Test") {
+	for res := range attacker.Attack(myTarget, rate, duration, "Load Test") {
 		metrics.Add(res)
 	}
 	metrics.Close()
